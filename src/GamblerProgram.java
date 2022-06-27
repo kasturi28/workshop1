3851043ffc0397617da588edda3560909b26eb1e
kasturi28 <kasturipansare2818@gmail.com>
2022-06-27 19:53:17 +0530

    Added user case 1

---
+++ src/GamblerProgram.java
@@ -0,0 +1,24 @@
+
+public class GamblerProgram {
+	/**
+	 * 
+	 * @param args
+	 */
+
+	public static void main(String[] args) {
+		/*
+		 * defining the variable stake , bet
+		 * display the welcome message
+		 * display the amount & bet statement for the gambler
+		 */
+
+		int amount = 100;
+		int bet = 1;
+
+		System.out.println("Welcome to the Gambler programme....");
+		System.out.println("total amount of the gambler = " + amount);
+		System.out.println("The gambler start with the amount = " + bet);
+
+	}
+
+}
